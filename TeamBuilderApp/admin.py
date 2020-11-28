26b31c7e9b093fa84cf0735502aedc768c73cff5
e6madeit <edwardifekoya1@outlook.com>
2020-11-28 17:35:24 +0000

    Changes to Admin site - 2

--- TeamBuilderApp/admin.py
+++ TeamBuilderApp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-	list_display = ('first_name', 'last_name', 'email')
+	list_display  = ('first_name', 'last_name', 'email')
+	search_fields = ('first_name', 'last_name')
 
 admin.site.register(User, UserAdmin)
